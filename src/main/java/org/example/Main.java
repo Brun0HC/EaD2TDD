1e9d6efcafb68a00bb19d6f1db58f1dba28936a0
Brun0HC <bruno.cobuci@biotronica.tech>
2024-04-14 13:22:55 -0300

    :sparkles: feat: Implemented main logics

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,32 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a LARGURA da página do álbum:");
+        int X = scanner.nextInt(); // largura pagina
+        System.out.println("Digite a ALTURA da página do álbum:");
+        int Y = scanner.nextInt(); // altura pagina
+
+        System.out.println("Digite a LARGURA da foto:");
+        int L = scanner.nextInt(); // largura foto
+        System.out.println("Digite a ALTURA da foto:");
+        int H = scanner.nextInt(); // altura da foto
+
+        String result = isPossible(X, Y, L, H);
+        System.out.println(result);
+    }
+
+    public static String isPossible(int X, int Y, int L, int H) {
+        if (2 * L <= X && H <= Y) {
+            return "S";
+        } else if (L <= X && 2 * H <= Y) {
+            return "S";
+        } else if(H+L <= Y || H+L<=X){
+            return "S";
+        }else{
+            return "N";
+        }
+    }
 
 }
